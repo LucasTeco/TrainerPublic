5e9be94d2b2742f0171be019f09cd74d970dd288
LucasTeco <laaguado@teco.com.ar>
2024-11-05 10:37:17 -0300

    Delete sync.ps1

--- sync.ps1
+++
@@ -1,9 +0,0 @@
-$from = "html-bootstrap-editor-showcase/src/*"
-$to = "shared/html-bootstrap-editor-showcase"
-
-try {
-    . ("..\sync\watcher.ps1")
-}
-catch {
-    Write-Host "Error while loading sync.ps1 script." 
-}
